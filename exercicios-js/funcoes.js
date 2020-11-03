702a8666ae490879498a2b80c185ca853136a035
Lúcio Flávio <31806245+luciosantos3@users.noreply.github.com>
2020-11-03 16:50:46 -0300

    Mais exemplos
    Adicionando exemplos de funções e variáveis.

--- exercicios-js/funcoes.js
+++ exercicios-js/funcoes.js
@@ -15,9 +15,18 @@ const somar = (a, b) => {
 }
 console.log(somar(1, 1))
 
+//Armazenando uma função em uma varaiável
+const imprimirSoma2 = function(a, b) {
+    console.log(a + b)
+}
+imprimirSoma2(2, 3)
+
 //Arrow function reduzida.
 //Função usada para instruções em apenas uma linha.
 const somar2 = (a, b) => a + b
 console.log(somar2(1, 5))
 
+//Arrow function ainda menor.
+const somar3 = a => console.log(a)
+imprimirSoma('Variável A')
 
